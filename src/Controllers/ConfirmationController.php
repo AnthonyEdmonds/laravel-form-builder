84ec34abe55ab2c3cf6b59d1317d977d863ab58c
Anthony Edmonds <anthony.edmonds@live.co.uk>
2025-12-10 10:56:06 +0000

    Removed check access from confirmation page

--- src/Controllers/ConfirmationController.php
+++ src/Controllers/ConfirmationController.php
@@ -11,7 +11,6 @@ class ConfirmationController extends Controller
     public function show(string $formKey): View
     {
         return Form::load($formKey)
-            ->checkAccess()
             ->confirmation()
             ->checkAccess()
             ->show();
